c19810684841218ace0140555d235f1876909c88
thierry-a <thierryarzal353@gmail.com>
2020-05-04 14:44:32 +0200

    First version of the Film Form

---
+++ src/FormFilm.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class FormFilm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          title: '',
+          poster: '',
+          comment: '',
+        }
+        this.onChange = this.onChange.bind(this);
+        this.submitForm = this.submitForm.bind(this);
+      }
+      
+    onChange(e) {
+      this.setState({
+        [e.target.name]: e.target.value,
+      });
+    }
+
+   submitForm(e) {
+      e.preventDefault();
+      const url = 'https://post-a-form.herokuapp.com/api/movies/';
+      axios.post(url, this.state)
+      .then(res => res.data)
+      .then(res => {
+        alert(`Film ajouté avec l'ID ${res.id} !`);
+      })
+      .catch(e => {
+        console.error(e);
+        alert(`Erreur lors de l'ajout d'un film : ${e.message}`);
+      });
+    }
+
+        
+    render() {
+        return (
+            <div className="FormFilm">
+              <h1>Saisie d'un film préféré</h1>
+                <form onSubmit={this.submitForm}>
+                    <fieldset>
+                      <legend>Informations</legend>
+                        <div className="form-data">
+                            <label htmlFor="title">Nom</label>
+                            <input
+                              type="text"
+                              id="title"
+                              name="title"
+                              onChange={this.onChange}
+                              value={this.state.title}
+                            />
+                        </div>
+
+                        <div className="form-data">
+                            <label htmlFor="poster">Poster: </label>
+                            <input
+                            type="text"
+                            id="poster"
+                             name="poster"
+                             onChange={this.onChange}
+                             value={this.state.poster}
+                             placeholder="http:// "
+                            />
+                        </div>
+
+                        <div className="form-data">
+                            <label htmlFor="comment">Comment: </label>
+                            <input
+                              type="textarea"
+                              id="comment"
+                              name="comment"
+                              onChange={this.onChange}
+                              value={this.state.comment}
+                            />
+                        </div>
+                     <hr />
+                        <div className="form-data">
+                            <input type="submit" value="Envoyer" />
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        );
+    }
+        
+}
+
+export default FormFilm;
